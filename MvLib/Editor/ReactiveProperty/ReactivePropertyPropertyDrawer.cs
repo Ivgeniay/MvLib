196a00514ef475a2f4414fcbf13e8c4fdd1442eb
Evgenii <motleycrue6502@gmail.com>
2024-09-26 14:27:29 +0400

    MvLib init

---
+++ MvLib/Editor/ReactiveProperty/ReactivePropertyPropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+using UnityEditor;
+using MvLib.Reactive;
+
+namespace MvLib
+{
+    [CustomPropertyDrawer(typeof(ReactiveProperty<>), true)]
+    internal class ReactivePropertyPropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var root = new VisualElement();
+            var valueProperty = property.FindPropertyRelative("value"); 
+            var field = new PropertyField(valueProperty, property.displayName);
+            root.Add(field);
+            return root;
+        }
+    }
+}
